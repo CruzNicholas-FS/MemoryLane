5b3c652fccbc0bf8302d89b2a082bdeee52abefa
Necolanch <NICruz@student.fullsail.edu>
2022-08-07 17:17:38 -0400

    Home page data begin first individual post page

---
+++ contentful.js
@@ -0,0 +1,68 @@
+const query = `
+{
+    blogPostCollection {
+        items {
+          sys {
+            firstPublishedAt
+          }
+          title
+          body{
+            json
+          }
+        }
+      }
+    homeCollection{
+      items {
+        devBlog
+        postLink
+        postLink2
+        postLink3
+      }
+    }
+  }
+`
+const fetchOptions = {
+  spaceID: "axq116fvwwfs",
+  accessToken: "wTUU18aBxQtJol5mtM5fWPFlBg7KIOn1Yj6yJ6zTVno",
+  endpoint: "https://graphql.contentful.com/content/v1/spaces/axq116fvwwfs",
+  method: "POST",
+  headers: {
+    Authorization: "Bearer wTUU18aBxQtJol5mtM5fWPFlBg7KIOn1Yj6yJ6zTVno",
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({ query })
+}
+
+const homePage = (posts) => {
+console.log(posts);
+
+const title = document.createElement("h1");
+
+const postLink1=document.createElement("a");
+const postLink2=document.createElement("a");
+const postLink3=document.createElement("a");
+
+const postTitle1=document.createElement("h3");
+const postTitle2=document.createElement("h3");
+const postTitle3=document.createElement("h3");
+
+title.innerHTML=posts.homeCollection.items[0].devBlog
+
+postTitle1.innerText=posts.blogPostCollection.items[0].title
+postTitle2.innerText=posts.blogPostCollection.items[1].title
+postTitle3.innerText=posts.blogPostCollection.items[2].title
+
+postLink1.href=posts.homeCollection.items[0].postLink
+postLink2.href=posts.homeCollection.items[0].postLink2
+postLink3.href=posts.homeCollection.items[0].postLink3
+
+postLink1.append(postTitle1);
+postLink2.append(postTitle2);
+postLink3.append(postTitle3);
+
+document.body.append(title, postLink1, postLink2, postLink3);
+}
+
+fetch(fetchOptions.endpoint, fetchOptions)
+.then(response => response.json())
+.then(data => homePage(data.data));
---
+++ post1.js
@@ -0,0 +1,42 @@
+const query = `
+{
+    blogPostCollection {
+        items {
+          sys {
+            firstPublishedAt
+          }
+          title
+          body{
+            json
+          }
+        }
+      }
+    homeCollection{
+      items {
+        devBlog
+        postLink
+        postLink2
+        postLink3
+      }
+    }
+  }
+`
+const fetchOptions = {
+  spaceID: "axq116fvwwfs",
+  accessToken: "wTUU18aBxQtJol5mtM5fWPFlBg7KIOn1Yj6yJ6zTVno",
+  endpoint: "https://graphql.contentful.com/content/v1/spaces/axq116fvwwfs",
+  method: "POST",
+  headers: {
+    Authorization: "Bearer wTUU18aBxQtJol5mtM5fWPFlBg7KIOn1Yj6yJ6zTVno",
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({ query })
+}
+
+const post1 = (posts) => {
+    console.log(posts)
+}
+
+fetch(fetchOptions.endpoint, fetchOptions)
+.then(response => response.json())
+.then(data => homePage(data.data.blogPostCollection));
